99304508f0844ebf333f77434631c14b3439eba6
Namazed <actek17@gmail.com>
2016-03-05 13:53:12 +0700

    Create MainActivity class and layout

---
+++ app/src/main/java/com/namazed/remindapp_project/MainActivity.java
@@ -0,0 +1,22 @@
+package com.namazed.remindapp_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        initToolbar();
+        initDrawer();
+    }
+
+    private void initToolbar() {
+    }
+
+    private void initDrawer() {
+    }
+}
